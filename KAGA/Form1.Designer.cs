24e5c8d61bed7b0c87ef54a18db81d6177f8ad07
Emre <xthemre@gmail.com>
2023-04-17 01:09:08 +0300

    Veri tabanı oluşturulup,ekleme sayfası form dizaynı yapıldı.

--- KAGA/Form1.Designer.cs
+++ KAGA/Form1.Designer.cs
@@ -37,13 +37,15 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(105, 263);
+            this.label1.Location = new System.Drawing.Point(49, 61);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 20);
             this.label1.TabIndex = 0;
@@ -53,7 +55,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(114, 319);
+            this.label2.Location = new System.Drawing.Point(78, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(42, 20);
             this.label2.TabIndex = 1;
@@ -61,7 +63,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(165, 395);
+            this.button1.Location = new System.Drawing.Point(111, 180);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 2;
@@ -70,14 +72,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(229, 260);
+            this.textBox1.Location = new System.Drawing.Point(173, 58);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(105, 27);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(230, 312);
+            this.textBox2.Location = new System.Drawing.Point(174, 110);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(104, 27);
             this.textBox2.TabIndex = 4;
@@ -85,9 +87,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(37, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(29, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(368, 188);
+            this.pictureBox1.Size = new System.Drawing.Size(413, 205);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -96,7 +98,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(114, 212);
+            this.label3.Location = new System.Drawing.Point(78, 10);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(178, 26);
             this.label3.TabIndex = 6;
@@ -104,32 +106,41 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(411, 12);
+            this.button2.Location = new System.Drawing.Point(448, 4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(25, 29);
             this.button2.TabIndex = 7;
             this.button2.Text = "X";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(67, 215);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 217);
+            this.panel1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 436);
+            this.ClientSize = new System.Drawing.Size(474, 436);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -143,5 +154,6 @@
         private PictureBox pictureBox1;
         private Label label3;
         private Button button2;
+        private Panel panel1;
     }
 }
---
+++ KAGA/Form2.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KAGA
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "200";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "201";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "202";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "203";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "204";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "205";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "206";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "207";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = "208";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mevcut durumdaki odalar müsaittir.");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mevcut odalar kullanılmaktadır.");
+        }
+    }
+}
