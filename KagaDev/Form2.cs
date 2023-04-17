6a4a04a55e31abf059e2e0a84a16e4a8db8506c0
Emre <xthemre@gmail.com>
2023-04-17 16:51:54 +0300

    Veri tabanı bağlantısı yapıldı ve insert fonksiyonu eklendi

--- KagaDev/Form2.cs
+++ KagaDev/Form2.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace KagaDev
 {
     public partial class Form2 : Form
@@ -16,7 +16,10 @@ namespace KagaDev
         {
             InitializeComponent();
         }
-
+        MySqlConnection conn = new MySqlConnection("Server=localhost;Database=kaga;Uid=root;Pwd='';");
+        MySqlCommand cmd;
+        MySqlDataAdapter adapter;
+        DataTable dt;
         private void button2_Click(object sender, EventArgs e)
         {
             textBox7.Text = "200";
@@ -60,6 +63,26 @@ namespace KagaDev
         private void button10_Click(object sender, EventArgs e)
         {
             textBox7.Text = "208";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Insert into ekle (Ad,Soyad,Cinsiyet,Telefon,Kan,tc,oda,giris,cikis) values (@ad,@soyad,@cinsiyet,@telefon,@kan,@tc,@oda,@giris,@cikis)";
+            cmd = new MySqlCommand(sorgu, conn);
+            cmd.Parameters.AddWithValue("@ad", textBox1.Text);
+            cmd.Parameters.AddWithValue("@soyad", textBox2.Text);
+            cmd.Parameters.AddWithValue("@cinsiyet", comboBox1.Text);
+            cmd.Parameters.AddWithValue("@telefon", textBox4.Text);
+            cmd.Parameters.AddWithValue("@kan", textBox5.Text);
+            cmd.Parameters.AddWithValue("@tc", textBox6.Text);
+            cmd.Parameters.AddWithValue("@oda", textBox7.Text);
+            cmd.Parameters.AddWithValue("@cikis", Convert.ToString(dateTimePicker1));
+            cmd.Parameters.AddWithValue("@cikis", Convert.ToInt32(dateTimePicker2));
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            VeriGetir();
+            MessageBox.Show("Kayıt Eklendi.");
         }
     }
 }
