0cc04712eb7e6d2c8e196c7e8d46f129d8e735af
Emre <xthemre@gmail.com>
2023-04-18 12:24:23 +0300

    güncelleme v1.0 database apache sıkıntısı çözüldü

--- KagaDev/form5.Designer.cs
+++ KagaDev/form5.Designer.cs
@@ -40,6 +40,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -132,11 +133,21 @@
             this.button4.Text = "Ara";
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(330, 141);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 63);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Ara";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(808, 455);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -163,5 +174,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
